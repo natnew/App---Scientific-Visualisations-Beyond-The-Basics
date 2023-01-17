b170a10f84815920dca0f3f4c0935b754d08d697
natnew <natashanewboldlondon@gmail.com>
2023-01-17 18:33:32 +0000

    Update Line Chart.py
    update

--- pages/Line Chart.py
+++ pages/Line Chart.py
@@ -58,7 +58,7 @@ def get_chart_42947925():
     import plotly.graph_objects as go
     import numpy as np
 
-    x = np.array([1, 2, 3, 4, 5])
+    x = np.array([1980, 1990, 2000, 2010, 2020])
     y = np.array([1, 3, 2, 3, 1])
 
     fig = go.Figure()
